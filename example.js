d70593379921976637eaf44d2ba21bc9bd20e40a
Dustin Deus <deusdustin@gmail.com>
2017-11-05 15:18:18 +0100

    ...

--- example.js
+++ example.js
@@ -4,13 +4,17 @@ const Bootme = require('./')
 
 const task = new Bootme.Task().setName('test')
 
-task.before(async function() {})
-task.action(async function() {})
-task.recover(async function() {})
-task.after(async function() {})
+task.addHook('onBefore', async function() {})
+task.addHook('onAfter', async function() {})
+task.addHook('onFailure', async function() {})
+task.action(async function() {
+  console.log('Foo')
+})
 
 const registry = new Bootme.Registry()
 registry.addTask(task)
+registry.addHook('test', 'onBefore', () => console.log('Before test'))
+registry.addHook('test', 'onAfter', () => console.log('After test'))
 
 const pipeline = new Bootme.Pipeline(registry)
 pipeline.execute()
